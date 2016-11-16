8e77b1fd35eacba130a5646d983eda7964f9a0d8
Sandeep Rawat <sandeep@opstree.com>
2016-11-16 19:27:26 +0530

    Added an isntruction to list out the commits

--- jenkins/jobdsl/Spring3HibernateAppReportGenerator.groovy
+++ jenkins/jobdsl/Spring3HibernateAppReportGenerator.groovy
@@ -4,6 +4,6 @@ job('Spring3HibernateAppReportGenerator') {
   customWorkspace('$JENKINS_HOME/data/Spring3HibernateApp')
 
   steps {
-    shell("echo 'I will generate the report for commits lying between' \n cat previous.commit current.commit \n souce ${JENKINS_HOME}/scripts/functions/git_functions.sh \n create_report_file_for_commits `cat previous.commit` `cat current.commit` \n cat list.commits ")
+    shell("echo 'I will generate the report for commits lying between' \n cat previous.commit current.commit \n source ${JENKINS_HOME}/scripts/functions/git_functions.sh \n create_report_file_for_commits `cat previous.commit` `cat current.commit` \n cat list.commits ")
   }
  }
