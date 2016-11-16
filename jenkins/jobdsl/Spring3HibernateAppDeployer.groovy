52e12dd12946a150a145e367ae6e3186b4e64ba7
Sandeep Rawat <sandeep@opstree.com>
2016-11-16 16:46:05 +0530

    Created a job to build the package for Spring3 App

---
+++ jenkins/jobdsl/Spring3HibernateAppDeployer.groovy
@@ -0,0 +1,17 @@
+mavenJob('Spring3HibernateAppDeployer') {
+	description("I\'ll deploy the application Spring3HibernateApp")
+    logRotator(-1, 10)
+	scm {
+   		git {
+     		branch('*/master')
+     		remote {
+       			name('')
+       			refspec('')
+       			url('https://github.com/OpsTree/ContinuousIntegration.git')
+     		}
+   		}
+ 	}
+ 	//please enable the maven in your jenkins form jenkins configuration
+ 	goals('clean install')
+ 	rootPOM('Spring3HibernateApp/pom.xml')
+}
