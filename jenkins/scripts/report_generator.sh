1308944d9bedcd70812968b3811ce9425beb5acc
Mohit <devops@ephesoft.com>
2016-11-20 05:18:31 -0500

    Added generation of table headers in html report

--- jenkins/scripts/report_generator.sh
+++ jenkins/scripts/report_generator.sh
@@ -7,12 +7,22 @@ function generateReport() {
   addBody
   startHTML
   startTable
+  startRow
+  addHeader "JIRA-ID"
+  addHeader "JIRA-Summary"
+  addHeader "JIRA-STATUS"
+  addHeader "JIRA-Reporter"
+  endRow
   while read -r jiraid
 	do
     startRow
     JIRA_TITLE=`get_jira_id_description ${jiraid}`
+    JIRA_STATUS=`get_jira_status_from_jira_id ${jiraid}`
+    JIRA_REPORTER=`get_jira_reporter_from_jira_id ${jiraid}`
     addCell "${jiraid}"
     addCell "${JIRA_TITLE}"
+    addCell "${JIRA_STATUS}"
+    addCell "${JIRA_REPORTER}"
     endRow
 	done < jiraid.commits
   endTable
